56d1636ceb75b8a1a0baa857a91437b696847284
Philipe Casarotte <philipe@dburnsdesign.com>
2009-05-08 19:15:43 -0300

    added childrens info page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,9 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post]) unless params[:post].blank?
     if @post.save
-      flash[:notice] = 'Sua mensagem foi enviada.'
+      flash.now[:notice] = 'Sua mensagem foi enviada.'
     else
-      flash[:error] =  "Sua mensagem não pode ser enviada."
+      flash.now[:error] =  "Sua mensagem não pode ser enviada."
     end
     @posts = Post.all
     render :partial => "posts"
